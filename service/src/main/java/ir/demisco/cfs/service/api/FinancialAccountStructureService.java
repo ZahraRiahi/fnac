b7f065185b4a0848d7d61bd607556ee0aecaffa6
m.kazemi <m.kazemi@istd.com>
2021-06-08 16:36:37 +0430

    feature-fin-113

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialAccountStructureService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialAccountStructureService.java
@@ -16,4 +16,5 @@ public interface FinancialAccountStructureService {
 
     FinancialAccountStructureDto update(FinancialAccountStructureDto financialAccountStructureDto);
 
+    Boolean deleteFinancialAccountStructureById(Long financialAccountStructure);
 }
