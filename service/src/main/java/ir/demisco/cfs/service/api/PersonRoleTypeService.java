ce95108dc48811673f88eaf0815fb8b9f01f4493
m.kazemi <m.kazemi@istd.com>
2021-06-16 15:03:41 +0430

    feature-fin-130

---
+++ service/src/main/java/ir/demisco/cfs/service/api/PersonRoleTypeService.java
@@ -0,0 +1,9 @@
+package ir.demisco.cfs.service.api;
+
+import ir.demisco.cfs.model.dto.response.PersonRoleTypeDto;
+
+import java.util.List;
+
+public interface PersonRoleTypeService {
+    List<PersonRoleTypeDto> getPersonRoleType();
+}
--- service/src/main/java/ir/demisco/cfs/service/repository/PersonRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/PersonRepository.java
@@ -4,4 +4,5 @@ import ir.demisco.cloud.basic.model.entity.prs.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
 }
