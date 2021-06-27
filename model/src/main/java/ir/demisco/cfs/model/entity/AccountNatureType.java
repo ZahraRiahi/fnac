f583129ca5cbbb66e82373fc9667fe77045f8a1c
m.kazemi <m.kazemi@istd.com>
2021-06-27 17:13:11 +0430

    feature-fin-155

--- model/src/main/java/ir/demisco/cfs/model/entity/AccountNatureType.java
+++ model/src/main/java/ir/demisco/cfs/model/entity/AccountNatureType.java
@@ -3,12 +3,14 @@ package ir.demisco.cfs.model.entity;
 import javax.persistence.Entity;
 import javax.persistence.*;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "account_nature_type", schema = "fnac")
 public class AccountNatureType {
     private Long id;
     private String description;
+    private LocalDateTime DeletedDate;
 
     @Id
     public Long getId() {
@@ -26,5 +28,14 @@ public class AccountNatureType {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Column(name = "DELETED_DATE")
+    public LocalDateTime getDeletedDate() {
+        return DeletedDate;
+    }
+
+    public void setDeletedDate(LocalDateTime deletedDate) {
+        DeletedDate = deletedDate;
     }
 }
---
+++ service/src/main/java/ir/demisco/cfs/service/repository/AccountNatureTypeRepository.java
@@ -0,0 +1,12 @@
+package ir.demisco.cfs.service.repository;
+
+import ir.demisco.cfs.model.entity.AccountNatureType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AccountNatureTypeRepository extends JpaRepository<AccountNatureType, Long> {
+    @Query(value = "select acnt from  AccountNatureType acnt  where  acnt.deletedDate is null ")
+    List<AccountNatureType> findByAccountNatureType();
+}
