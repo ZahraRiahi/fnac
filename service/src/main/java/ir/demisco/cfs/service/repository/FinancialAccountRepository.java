c278f96797638255fc074929174e3e08ecafcd5e
m.kazemi <m.kazemi@istd.com>
2021-07-11 13:26:51 +0430

    feature-fin-154

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountRepository.java
@@ -1,6 +1,7 @@
 package ir.demisco.cfs.service.repository;
 
 import ir.demisco.cfs.model.entity.FinancialAccount;
+import ir.demisco.cfs.model.entity.FinancialAccountStructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,5 +11,9 @@ import java.util.List;
 public interface FinancialAccountRepository extends JpaRepository<FinancialAccount, Long> {
     @Query(value = " select fa from  FinancialAccount fa where fa.organization.id=:organizationId")
     List<FinancialAccount> findByFinancialAccountByOrganizationId(Long organizationId);
+
+    @Query("select fa from  FinancialAccount fa where fa.financialAccountStructure.id=:financialAccountStructureId and fa.deletedDate is null")
+    List<FinancialAccount> findByFinancialAccountStructureId(Long financialAccountStructureId);
+
 
 }
