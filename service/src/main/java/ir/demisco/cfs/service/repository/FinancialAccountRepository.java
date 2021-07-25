73fba286c7d508a839b657ae841ae82cb685d8bd
m.kazemi <m.kazemi@istd.com>
2021-07-25 12:51:17 +0430

    feature-fin-209

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountRepository.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 
 public interface FinancialAccountRepository extends JpaRepository<FinancialAccount, Long> {
-    @Query(value = " select fa from  FinancialAccount fa where fa.organization.id=:organizationId and fa.deletedDate is null")
+    @Query(value = " select fa from  FinancialAccount fa join fa.financialAccountStructure fs where fs.sequence = (select max(fs_inner.sequence) from  FinancialAccountStructure  fs_inner " +
+            " where fs_inner.financialCodingType.id=fs.financialCodingType.id) " +
+            " and fa.organization.id=:organizationId and fa.deletedDate is null")
     List<FinancialAccount> findByFinancialAccountByOrganizationId(Long organizationId);
 
     @Query("select fa from  FinancialAccount fa where fa.financialAccountStructure.id=:financialAccountStructureId and fa.deletedDate is null")
