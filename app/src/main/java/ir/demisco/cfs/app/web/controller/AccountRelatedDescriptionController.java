055692261ff3cdac204d61c6c11c6120243711c1
m.kazemi <m.kazemi@istd.com>
2021-08-02 11:59:49 +0430

    delete account Related description

---
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/AccountRelatedDescriptionController.java
@@ -0,0 +1,27 @@
+package ir.demisco.cfs.app.web.controller;
+
+import ir.demisco.cfs.service.api.AccountRelatedDescriptionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api-accountRelatedDescription")
+public class AccountRelatedDescriptionController {
+    private final AccountRelatedDescriptionService accountRelatedDescriptionService;
+
+    public AccountRelatedDescriptionController(AccountRelatedDescriptionService accountRelatedDescriptionService) {
+        this.accountRelatedDescriptionService = accountRelatedDescriptionService;
+    }
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") Long accountRelatedDescriptionId) {
+        boolean result;
+        result = accountRelatedDescriptionService.deleteAccountRelatedDescriptionById(accountRelatedDescriptionId);
+        return ResponseEntity.ok(result);
+
+    }
+
+}
