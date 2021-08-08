919956e5eff4be13f9e5b3b462e763d81674d8ff
m.kazemi <m.kazemi@istd.com>
2021-08-08 16:28:09 +0430

    list Financial account Relation

---
+++ service/src/main/java/ir/demisco/cfs/service/repository/AccountMoneyTypeRepository.java
@@ -0,0 +1,7 @@
+package ir.demisco.cfs.service.repository;
+
+import ir.demisco.cfs.model.entity.AccountMoneyType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountMoneyTypeRepository extends JpaRepository<AccountMoneyType, Long> {
+}
