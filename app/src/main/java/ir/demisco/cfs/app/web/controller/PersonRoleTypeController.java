7c5290d1838ca64b0e521832a75a9d04a82c72df
m.kazemi <m.kazemi@istd.com>
2021-08-14 17:27:27 +0430

    feature-fin-288

--- app/src/main/java/ir/demisco/cfs/app/web/controller/PersonRoleTypeController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/PersonRoleTypeController.java
@@ -1,16 +1,18 @@
 package ir.demisco.cfs.app.web.controller;
 
+import ir.demisco.cfs.model.dto.request.PersonRoleListRequest;
 import ir.demisco.cfs.model.dto.response.PersonRoleTypeDto;
 import ir.demisco.cfs.service.api.PersonRoleTypeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-person_role_type")
+@RequestMapping("/api-personRoleType")
 public class PersonRoleTypeController {
     private final PersonRoleTypeService personRoleTypeService;
 
@@ -19,7 +21,9 @@ public class PersonRoleTypeController {
     }
 
     @PostMapping("/list")
-    public ResponseEntity<List<PersonRoleTypeDto>> responseEntity() {
-        return ResponseEntity.ok(personRoleTypeService.getPersonRoleType());
+    public ResponseEntity<List<PersonRoleTypeDto>> responseEntity(@RequestBody PersonRoleListRequest personRoleListRequest) {
+        return ResponseEntity.ok(personRoleTypeService.getPersonRoleType(personRoleListRequest));
     }
+
+
 }
