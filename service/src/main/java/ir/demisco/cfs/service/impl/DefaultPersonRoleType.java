97151740376f1efd03353ec6eda9d46b335193f8
m.kazemi <m.kazemi@istd.com>
2021-08-14 17:27:27 +0430

    feature-fin-288

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultPersonRoleType.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultPersonRoleType.java
@@ -1,13 +1,14 @@
 package ir.demisco.cfs.service.impl;
 
+import ir.demisco.cfs.model.dto.request.PersonRoleListRequest;
 import ir.demisco.cfs.model.dto.response.PersonRoleTypeDto;
 import ir.demisco.cfs.service.api.PersonRoleTypeService;
 import ir.demisco.cfs.service.repository.PersonRoleTypeRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DefaultPersonRoleType implements PersonRoleTypeService {
@@ -18,16 +19,34 @@ public class DefaultPersonRoleType implements PersonRoleTypeService {
     }
 
     @Override
-    @Transactional
-    public List<PersonRoleTypeDto> getPersonRoleType() {
-        List<Object[]> personRoleTypeList = personRoleTypeRepository.findByPersonRoleTypeListObject();
-        List<PersonRoleTypeDto> personRoleTypeDtos = new ArrayList<>();
-        personRoleTypeList.forEach(objects -> {
-            PersonRoleTypeDto personRoleTypeDto = PersonRoleTypeDto.builder().id(Long.parseLong(objects[0].toString()))
-                    .description(objects[1].toString()).flagExist(Long.parseLong(objects[2].toString())).build();
-            personRoleTypeDtos.add(personRoleTypeDto);
-        });
-        return personRoleTypeDtos;
-
+    @Transactional(rollbackOn = Throwable.class)
+    public List<PersonRoleTypeDto> getPersonRoleType(PersonRoleListRequest personRoleListRequest) {
+        Object centricAccount;
+        if (personRoleListRequest.getCentricAccountId() != null) {
+            centricAccount = "centricAccount";
+        } else {
+            personRoleListRequest.setCentricAccountId(0L);
+            centricAccount = null;
+        }
+        List<Object[]> centricAccountTypeListObject = personRoleTypeRepository.findByPersonRoleTypeListObject(centricAccount, personRoleListRequest.getCentricAccountId());
+        return centricAccountTypeListObject.stream().map(objects -> PersonRoleTypeDto.builder().id(Long.parseLong(objects[0].toString()))
+                .description(objects[1].toString())
+                .flagExist(Long.parseLong(objects[2].toString()))
+                .build()).collect(Collectors.toList());
     }
+
+
+//    @Override
+//    @Transactional
+//    public List<PersonRoleTypeDto> getPersonRoleType() {
+//        List<Object[]> personRoleTypeList = personRoleTypeRepository.findByPersonRoleTypeListObject();
+//        List<PersonRoleTypeDto> personRoleTypeDtos = new ArrayList<>();
+//        personRoleTypeList.forEach(objects -> {
+//            PersonRoleTypeDto personRoleTypeDto = PersonRoleTypeDto.builder().id(Long.parseLong(objects[0].toString()))
+//                    .description(objects[1].toString()).flagExist(Long.parseLong(objects[2].toString())).build();
+//            personRoleTypeDtos.add(personRoleTypeDto);
+//        });
+//        return personRoleTypeDtos;
+//
+//    }
 }
--- service/src/main/java/ir/demisco/cfs/service/repository/PersonRoleTypeRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/PersonRoleTypeRepository.java
@@ -11,4 +11,28 @@ public interface PersonRoleTypeRepository extends JpaRepository<PersonRoleType, 
             " where prt.deletedDate is null ")
     List<Object[]> findByPersonRoleTypeListObject();
 
+
+    @Query(value = "select prty.id," +
+            " prty.description," +
+            " case" +
+            " when :centricAccountId is null or not exists " +
+            " (select 1 " +
+            " from fnac.centric_person_role cnpr " +
+            " where (:centricAccount is null or " +
+            " cnpr.centric_account_id = :centricAccountId)" +
+            " and cnpr.person_role_type_id = prty.id" +
+            " and cnpr.deleted_date is null) then " +
+            " 0 " +
+            " else " +
+            " 1 " +
+            " end flg_exists " +
+            " from fnac.person_role_type prty " +
+            " where prty.deleted_date is null "
+            , nativeQuery = true)
+    List<Object[]> findByPersonRoleTypeListObject(Object centricAccount, Long centricAccountId);
+
+
+
+
+
 }
