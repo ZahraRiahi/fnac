c7bb7c95118494e3782104d0a450b3608bca2e55
m.kazemi <m.kazemi@istd.com>
2021-08-14 09:47:23 +0430

    feature-fin-303

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountTypeRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialAccountTypeRepository.java
@@ -32,5 +32,24 @@ public interface FinancialAccountTypeRepository extends JpaRepository<FinancialA
             , nativeQuery = true)
     List<Object[]> findByFinancialAccountAndFinancialAccountId(Object financialAccount, Long financialAccountId);
 
+
+    @Query(value = "select fnat.id,fnat.code," +
+            " fnat.description," +
+            " case" +
+            " when :financialAccountId is null or not exists " +
+            " (select 1 " +
+            " from fnac.account_related_type acrt " +
+            " where acrt.financial_account_type_id = fnat.id " +
+            " and acrt.financial_account_id = :financialAccountId " +
+            " and acrt.deleted_date is null) then " +
+            " 0 " +
+            " else " +
+            " 1 " +
+            " end flg_exists " +
+            " from fnac.financial_account_type fnat " +
+            " where fnat.deleted_date is null "
+            , nativeQuery = true)
+    List<Object[]> findByFinancialAccount(Long financialAccountId);
+
 }
 
