f15f60cfd10fc2a4c2edb4180b917f7337fdf553
m.kazemi <m.kazemi@istd.com>
2021-08-30 12:36:43 +0430

    faeture-fin-416

--- service/src/main/java/ir/demisco/cfs/service/repository/AccountDefaultValueRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/AccountDefaultValueRepository.java
@@ -8,8 +8,11 @@ import java.util.List;
 
 public interface AccountDefaultValueRepository extends JpaRepository<AccountDefaultValue, Long> {
     @Query("select count(adv.id) from AccountDefaultValue adv join adv.financialAccount fa " +
-            " where fa.id=:financialAccountId  and adv.accountRelationTypeDetail.id=:accountRelationTypeDetailId and adv.centricAccount.id=:centricAccountId and adv.deletedDate is null")
-    Long findByAccountDefaultAndfinancialAccountAndAccountRelationTypeDetailId(Long accountRelationTypeDetailId, Long financialAccountId, Long centricAccountId);
+            " where fa.id=:financialAccountId  and adv.accountRelationTypeDetail.id=:accountRelationTypeDetailId " +
+            " and (:centricAccount is null or " +
+            " adv.centricAccount.id = :centricAccountId)" +
+            " and adv.deletedDate is null")
+    Long findByAccountDefaultAndfinancialAccountAndAccountRelationTypeDetailId(Long accountRelationTypeDetailId, Long financialAccountId, Long centricAccountId, Object centricAccount);
 
     @Query("select count(adv.id)  from AccountDefaultValue adv where adv.financialAccount.id=:financialAccountId and adv.accountRelationTypeDetail.id in (:accountRelationTypeDetailIdList) ")
     Long findByFinancialAccountIdAndAccountRelationTypeDetailIdList(Long financialAccountId, List<Long> accountRelationTypeDetailIdList);
@@ -17,7 +20,7 @@ public interface AccountDefaultValueRepository extends JpaRepository<AccountDefa
     AccountDefaultValue findByIdAndAccountRelationTypeDetailId(Long id, Long accountRelationTypeDetailId);
 
     @Query("select adv  from AccountDefaultValue adv where  adv.id in (:accountDefaultValueIdList) ")
-    List<AccountDefaultValue> findAccountDefaultValueByFinancialAccount( List<Long> accountDefaultValueIdList);
+    List<AccountDefaultValue> findAccountDefaultValueByFinancialAccount(List<Long> accountDefaultValueIdList);
 
 
 }
