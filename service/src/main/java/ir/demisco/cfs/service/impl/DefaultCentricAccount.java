b2f72347eb6fa779c02fb6fd3ce840eb27f132c6
m.kazemi <m.kazemi@istd.com>
2021-09-29 16:52:34 +0330

    feature-fin-572

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultCentricAccount.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultCentricAccount.java
@@ -52,7 +52,7 @@ public class DefaultCentricAccount implements CentricAccountService {
     @Override
     @Transactional
     public List<CentricAccountNewResponse> getCentricAccountByOrganizationIdAndCentricAccountTypeId(Long centricAccountTypeId, Long organizationId) {
-        List<CentricAccount> centricAccountList = centricAccountRepository.findByCentricAccountAndOrganizationId(centricAccountTypeId, 100L);
+        List<CentricAccount> centricAccountList = centricAccountRepository.findByCentricAccountAndOrganizationId(centricAccountTypeId, SecurityHelper.getCurrentUser().getOrganizationId());
         return centricAccountList.stream().map(e -> CentricAccountNewResponse.builder().id(e.getId())
                 .name(e.getName())
                 .code(e.getCode()).build()).collect(Collectors.toList());
