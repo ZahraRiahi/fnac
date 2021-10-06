367b41549503b979079b9ba77f298fecac89bc63
m.kazemi <m.kazemi@istd.com>
2021-10-06 16:38:06 +0330

    feature-fin-593

--- service/src/main/java/ir/demisco/cfs/service/repository/AccountStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.demisco.cfs.service.repository;
-
-import ir.demisco.cfs.model.entity.AccountStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountStatusRepository extends JpaRepository<AccountStatus, Long> {
-}
