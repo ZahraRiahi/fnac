26ee93cad7aa4a0623e769f831c35aee35d1663f
m.kazemi <m.kazemi@istd.com>
2021-10-09 11:04:45 +0330

    feature-fin-596

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialAccountService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialAccountService.java
@@ -1,13 +1,11 @@
 package ir.demisco.cfs.service.api;
 
-import ir.demisco.cfs.model.dto.request.CentricAccountNewRequest;
 import ir.demisco.cfs.model.dto.request.FinancialAccountNewRequest;
 import ir.demisco.cfs.model.dto.request.FinancialAccountRequest;
 import ir.demisco.cfs.model.dto.request.FinancialAccountStatusRequest;
 import ir.demisco.cfs.model.dto.response.*;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
