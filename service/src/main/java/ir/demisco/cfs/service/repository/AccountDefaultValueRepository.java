d82da43ba9c356d09b8fd3ae28ab92eb7870ffac
m.kazemi <m.kazemi@istd.com>
2021-11-02 15:34:43 +0330

    feature-fin-673

--- service/src/main/java/ir/demisco/cfs/service/repository/AccountDefaultValueRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/AccountDefaultValueRepository.java
@@ -13,9 +13,9 @@ public interface AccountDefaultValueRepository extends JpaRepository<AccountDefa
             " and (:centricAccount is null or " +
             " adv.centricAccount.id = :centricAccountId)" +
             " and adv.deletedDate is null")
-    Long findByAccountDefaultAndfinancialAccountAndAccountRelationTypeDetailId(Long accountRelationTypeDetailId, Long financialAccountId, Long centricAccountId, Object centricAccount);
+    Long findByAccountDefaultAndFinancialAccountAndAccountRelationTypeDetailId(Long accountRelationTypeDetailId, Long financialAccountId, Long centricAccountId, Object centricAccount);
 
-    @Query("select count(adv.id)  from AccountDefaultValue adv where adv.financialAccount.id=:financialAccountId and adv.accountRelationTypeDetail.id in (:accountRelationTypeDetailIdList) ")
+    @Query("select count(adv.id)  from AccountDefaultValue adv where adv.financialAccount.id=:financialAccountId and adv.accountRelationTypeDetail.id in (:accountRelationTypeDetailIdList) and adv.deletedDate is null ")
     Long findByFinancialAccountIdAndAccountRelationTypeDetailIdList(Long financialAccountId, List<Long> accountRelationTypeDetailIdList);
 
     AccountDefaultValue findByIdAndAccountRelationTypeDetailId(Long id, Long accountRelationTypeDetailId);
--- service/src/main/java/ir/demisco/cfs/service/repository/AccountStructureLevelRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/AccountStructureLevelRepository.java
@@ -54,23 +54,49 @@ public interface AccountStructureLevelRepository extends JpaRepository<AccountSt
 //            "                             where fnas_inner2.id = :financialAccountStructureId" +
 //            "                               and fnas_inner2.deleted_date is null)))))"
 //            , nativeQuery = true)
-    @Query(value = " WITH STRUCTURE_QUERY AS " +
-            "   (SELECT T.ID, " +
-            "           T.FINANCIAL_ACCOUNT_STRUCTURE_ID, " +
-            "           T.CODE, " +
-            "           T.FINANCIAL_ACCOUNT_PARENT_ID " +
-            "      FROM fnac.FINANCIAL_ACCOUNT T " +
-            "     WHERE T.DELETED_DATE IS NULL " +
-            "     START WITH T.ID = :financialAccountId " +
-            "    CONNECT BY PRIOR T.FINANCIAL_ACCOUNT_PARENT_ID = T.ID " +
-            "     ORDER BY T.ID ASC) " +
-            "  SELECT  " +
-            "         ROWNUM, " +
-            "         s.CODE, " +
-            "         s.FINANCIAL_ACCOUNT_STRUCTURE_ID,         " +
-            "         :financialAccountId,        " +
-            "         s.FINANCIAL_ACCOUNT_PARENT_ID  " +
-            "    FROM STRUCTURE_QUERY s"
+//    @Query(value = " WITH STRUCTURE_QUERY AS " +
+//            "   (SELECT T.ID, " +
+//            "           T.FINANCIAL_ACCOUNT_STRUCTURE_ID, " +
+//            "           T.CODE, " +
+//            "           T.FINANCIAL_ACCOUNT_PARENT_ID " +
+//            "      FROM fnac.FINANCIAL_ACCOUNT T " +
+//            "     WHERE T.DELETED_DATE IS NULL " +
+//            "     START WITH T.ID = :financialAccountId " +
+//            "    CONNECT BY PRIOR T.FINANCIAL_ACCOUNT_PARENT_ID = T.ID " +
+//            "     ORDER BY T.ID ASC) " +
+//            "  SELECT  " +
+//            "         ROWNUM, " +
+//            "         s.CODE, " +
+//            "         s.FINANCIAL_ACCOUNT_STRUCTURE_ID,         " +
+//            "         :financialAccountId,        " +
+//            "         s.FINANCIAL_ACCOUNT_PARENT_ID  " +
+//            "    FROM STRUCTURE_QUERY s"
+//            , nativeQuery = true)
+    @Query(value = " SELECT SEQUENCE, " +
+            "       nvl(CODE, " +
+            "           (SELECT CODE " +
+            "              FROM fnac.FINANCIAL_ACCOUNT AC_INER " +
+            "             WHERE AC_INER.ID = :financialAccountId)), " +
+            "       FINANCIAL_ACCOUNT_STRUCTURE_ID, " +
+            "       FINANCIAL_ACCOUNT_PARENT_ID " +
+            "  FROM (SELECT AC.ID," +
+            "               nvl(AST_P.ID, ast.id) FINANCIAL_ACCOUNT_STRUCTURE_ID," +
+            "               AC_P.CODE CODE," +
+            "               NVL(AC.FINANCIAL_ACCOUNT_PARENT_ID, :financialAccountId) FINANCIAL_ACCOUNT_PARENT_ID," +
+            "               NVL(AST_P.SEQUENCE, AST.SEQUENCE) SEQUENCE" +
+            "          FROM fnac.FINANCIAL_ACCOUNT AC" +
+            "          LEFT OUTER JOIN fnac.FINANCIAL_ACCOUNT AC_P" +
+            "            ON AC_P.ID = AC.FINANCIAL_ACCOUNT_PARENT_ID" +
+            "          LEFT OUTER JOIN fnac.FINANCIAL_ACCOUNT_STRUCTURE AST_P" +
+            "            ON AST_P.ID = AC_P.FINANCIAL_ACCOUNT_STRUCTURE_ID" +
+            "         inner JOIN fnac.FINANCIAL_ACCOUNT_STRUCTURE AST" +
+            "            ON AST.ID = (SELECT FA_INER.FINANCIAL_ACCOUNT_STRUCTURE_ID" +
+            "                           FROM fnac.FINANCIAL_ACCOUNT FA_INER" +
+            "                          WHERE FA_INER.ID = :financialAccountId)" +
+            "         WHERE AC.DISABLE_DATE IS NULL" +
+            "         START WITH AC.ID = :financialAccountId" +
+            "        CONNECT BY PRIOR AC.FINANCIAL_ACCOUNT_PARENT_ID = AC.ID" +
+            "         ORDER BY NVL(AST_P.SEQUENCE, AST.SEQUENCE)) "
             , nativeQuery = true)
     List<Object[]> findByFinancialAccountStructureListObject(Long financialAccountId);
 
