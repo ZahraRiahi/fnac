1348a5831c4082680e585afcabae1b845e36e96e
m.kazemi <m.kazemi@istd.com>
2021-12-18 09:10:29 +0330

    feature-fin-871

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentItemRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentItemRepository.java
@@ -10,10 +10,18 @@ public interface FinancialDocumentItemRepository extends JpaRepository<Financial
 
     @Query(" select count(fdi.id) from FinancialDocumentItem fdi where fdi.financialAccount.id=:financialAccountId ")
     Long getFinancialDocumentItemByFinancialAccountId(Long financialAccountId);
+
     @Query(value = "   SELECT 1 FROM FNDC.FINANCIAL_DOCUMENT_ITEM DI " +
             "                        INNER JOIN fnac.FINANCIAL_ACCOUNT FA " +
             "                              ON FA.ID = DI.FINANCIAL_ACCOUNT_ID " +
             "                              AND FA.FINANCIAL_ACCOUNT_STRUCTURE_ID = :financialAccountStructureId AND DI.DELETED_DATE IS NULL  "
             , nativeQuery = true)
     List<Object> findByFinancialDocumentAndFinancialAccountStructure(Long financialAccountStructureId);
+
+    @Query(value = " SELECT 1 " +
+            "  FROM fndc.FINANCIAL_DOCUMENT_ITEM FDI " +
+            " WHERE FDI.FINANCIAL_ACCOUNT_ID = :financialAccountId " +
+            "   AND FDI.DELETED_DATE IS NULL  "
+            , nativeQuery = true)
+    List<Long> findByFinancialDocumentItemByFinancialAccountId(Long financialAccountId);
 }
