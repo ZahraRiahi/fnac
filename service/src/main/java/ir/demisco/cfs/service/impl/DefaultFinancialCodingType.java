df4c8db751ec1929f220b38b42c90175f973e2b7
m.kazemi <m.kazemi@istd.com>
2022-03-07 16:48:40 +0330

    feature-fin-1164

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialCodingType.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialCodingType.java
@@ -2,7 +2,6 @@ package ir.demisco.cfs.service.impl;
 
 import ir.demisco.cfs.model.dto.response.FinancialCodingTypeDto;
 import ir.demisco.cfs.model.dto.response.FinancialCodingTypeResponse;
-import ir.demisco.cfs.model.entity.CodingTypeOrgRel;
 import ir.demisco.cfs.model.entity.FinancialAccountStructure;
 import ir.demisco.cfs.model.entity.FinancialCodingType;
 import ir.demisco.cfs.service.api.FinancialCodingTypeService;
