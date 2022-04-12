c6f545e1127fdc0158122cbadd2fa7bd08466239
m.kazemi <m.kazemi@istd.com>
2022-04-12 15:10:58 +0430

    feature-fin-1212

--- app/src/main/java/ir/demisco/cfs/app/web/controller/CentricPersonRoleController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/CentricPersonRoleController.java
@@ -4,7 +4,10 @@ import ir.demisco.cfs.model.dto.request.CentricPersonRoleRequest;
 import ir.demisco.cfs.model.dto.response.CentricPersonRoleResponce;
 import ir.demisco.cfs.service.api.CentricPersonRoleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
