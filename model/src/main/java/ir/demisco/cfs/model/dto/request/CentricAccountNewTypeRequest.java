97a6af0205c67f5f89121a3bc8cc462c41fd7d7e
m.kazemi <m.kazemi@istd.com>
2022-05-14 11:20:03 +0430

    feature-fin-1303

--- model/src/main/java/ir/demisco/cfs/model/dto/request/CentricAccountNewTypeRequest.java
+++ model/src/main/java/ir/demisco/cfs/model/dto/request/CentricAccountNewTypeRequest.java
@@ -3,6 +3,8 @@ package ir.demisco.cfs.model.dto.request;
 public class CentricAccountNewTypeRequest {
     private Long centricAccountTypeId;
     private Long organizationId;
+    private String code;
+    private String name;
 
     public Long getCentricAccountTypeId() {
         return centricAccountTypeId;
@@ -18,5 +20,21 @@ public class CentricAccountNewTypeRequest {
 
     public void setOrganizationId(Long organizationId) {
         this.organizationId = organizationId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
