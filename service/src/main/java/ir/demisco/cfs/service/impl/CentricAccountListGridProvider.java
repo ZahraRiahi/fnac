1906ac9c2f3d9ed29180123467583a9cf8e4d541
m.kazemi <m.kazemi@istd.com>
2022-05-17 11:07:28 +0430

    feature-fin-1293

--- service/src/main/java/ir/demisco/cfs/service/impl/CentricAccountListGridProvider.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/CentricAccountListGridProvider.java
@@ -2,18 +2,11 @@ package ir.demisco.cfs.service.impl;
 
 import ir.demisco.cfs.model.dto.response.CentricAccountDto;
 import ir.demisco.cfs.model.entity.CentricAccount;
-import ir.demisco.cfs.model.entity.CentricOrgRel;
-import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.service.business.api.core.GridDataProvider;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Selection;
-import javax.persistence.criteria.Subquery;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
