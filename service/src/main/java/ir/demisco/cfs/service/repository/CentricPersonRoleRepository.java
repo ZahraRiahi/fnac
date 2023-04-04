dc5e9dea52678564446313bf58c40d35b5dd0def
m.kazemi <m.kazemi@istd.com>
2023-04-04 13:31:32 +0330

    feature-fin-2037

--- service/src/main/java/ir/demisco/cfs/service/repository/CentricPersonRoleRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/CentricPersonRoleRepository.java
@@ -9,5 +9,12 @@ import java.util.List;
 public interface CentricPersonRoleRepository extends JpaRepository<CentricPersonRole, Long> {
     @Query("select cpr from  CentricPersonRole cpr where cpr.centricAccount.id=:centricAccountId ")
     List<CentricPersonRole> findByCentricAccountId(Long centricAccountId);
+
+    @Query(value = " select 1" +
+            "  from fnac.centric_person_role t" +
+            " where t.centric_account_id = :centricAccountId" +
+            "   and t.person_role_type_id = :personRoleTypeId "
+            , nativeQuery = true)
+    List<Long> findByCentricPersonRoleByIdAndCentricId(Long centricAccountId, Long personRoleTypeId);
 }
 
